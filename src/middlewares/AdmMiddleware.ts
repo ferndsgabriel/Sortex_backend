4271219dbf2e635d0cd81ccd40d4f216ec11325d
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-08-16 14:55:44 -0300

    melhorando explicações sobre a diferença entre os subs

--- src/middlewares/AdmMiddleware.ts
+++ src/middlewares/AdmMiddleware.ts
@@ -16,12 +16,16 @@ export default async function AdmMiddleware(req: Request, res: Response, next: N
 
     try {
         const { sub } = verify(token, process.env.UJWT_ADM as string) as Payload; // tento pegar o sub dentro do token
+        //OBS: esse sub tem nada haver com o do google, ele é um dos 3 elementos que existe dentro do token
+        // 1 payload/ 2 obj hash e  3 sub, o sub é aonde envio o id do adm e agr estou usando para pega-lo
 
         if (!sub) {
             return res.status(401).send('Rota não autorizada').end();
         } // se n tiver sub n deixo seguir
 
         req.adm_id = sub; // se tiver crio o uma tipagem do express que passando o id que mandei no sub
+        // criar essa tipagem de adm_id para o request, me permite executar qualquer CRUD no banco de dados
+        // que envolva o id desse adm sem precisar que fique digitando o id, apenas extraindo direto do token de authenticação
 
         return next(); // deixo seguir
     } catch (err) {
