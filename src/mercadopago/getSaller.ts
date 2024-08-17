d78dbf1962fa6fcdd56c8cfa0a7d1fd17bbc6004
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-08-17 14:59:44 -0300

    try: tentando ver oque recebo direto no authCode

--- src/mercadopago/getSaller.ts
+++ src/mercadopago/getSaller.ts
@@ -10,15 +10,7 @@ class GetSaller{
             res.status(400).send('Código de autorização não encontrado.');
             return; // se n tiver um código quando eu chegar nessa rota, significa que o adm n viculou sua conta 
         }
-
-        try {
-            const accessToken = await getAccessToken(authCode); // Troca o authorization code pelo access token
-            console.log(`Autorização completa! O access_token é: ${accessToken}`)
-            res.send(`Autorização completa! O access_token é: ${accessToken}`);
-        } catch (error) {
-            console.error('Erro ao obter o access_token:', error);
-            res.status(500).send(`Erro ao processar a autorização: ${error}`);
-        }
+        console.log(authCode)
     }
 }
 
