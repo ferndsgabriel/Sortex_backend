88c3e0e142397d9c0c6017efdf314559454d8306
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-08-17 14:54:58 -0300

    try: tentando passar os tokens direto, sem env

--- src/mercadopago/axiosToken.ts
+++ src/mercadopago/axiosToken.ts
@@ -3,7 +3,13 @@ import 'dotenv/config';
 
 async function getAccessToken(authCode: string): Promise<string> {
     try {
-        const response = await axios.post('https://auth.mercadopago.com.br/authorization?client_id=3875468438633898&response_type=code&platform_id=mp&redirect_uri=https://sortexbackend.vercel.app/sallercallback')
+        const response = await axios.post('https://api.mercadopago.com/oauth/token', {
+            client_id:'3875468438633898',
+            client_secret: 'eq3X60nI9yfXl2lixEIa6ITMfM7HbmYS',
+            grant_type: 'authorization_code',
+            code: authCode,
+            redirect_uri: `${process.env.BASE_URL}/sallercallback`
+        });
 
         return response.data.access_token;
     } catch (error) {
