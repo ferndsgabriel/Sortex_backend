229bf68937959ec0ecf12e7627df5837a2172d0f
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-08-17 15:36:41 -0300

    change: mudando a forma de pegar erros e sucessos

--- src/mercadopago/axiosToken.ts
+++ src/mercadopago/axiosToken.ts
@@ -1,29 +1,26 @@
 import axios from 'axios';
-import qs from 'qs';  // Biblioteca para serializar dados em formato URL-encoded
 
 const clientId = '3875468438633898';
 const clientSecret = 'eq3X60nI9yfXl2lixEIa6ITMfM7HbmYS';
 
 const getAccessToken = async (authCode: string) => {
     try {
-        const response = await axios.post('https://api.mercadopago.com/oauth/token', qs.stringify({
-            grant_type: 'authorization_code',
+        const response = await axios.post('https://api.mercadopago.com/oauth/token', {
+            client_id: clientId,
+            client_secret: clientSecret,
             code: authCode,
+            grant_type: 'authorization_code',
             redirect_uri: 'https://sortexbackend.vercel.app/sallercallback'
-        }), {
-            auth: {
-                username: clientId,
-                password: clientSecret
-            },
+        }, {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
+                'Content-Type': 'application/json'
             }
         });
 
         return response.data.access_token;
     } catch (error) {
         console.error('Error getting access token:', error.response ? error.response.data : error.message);
-        throw error;  // Re-throw the error to be caught in the caller
+        throw error; // Re-throw the error to be caught in the caller
     }
 };
 
