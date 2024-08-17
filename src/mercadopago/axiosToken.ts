d5dee98758cfb1741a96e31ff9b51c6018c1997f
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-08-17 14:45:22 -0300

    fix: obter o código no params e não no body

--- src/mercadopago/axiosToken.ts
+++ src/mercadopago/axiosToken.ts
@@ -3,19 +3,21 @@ import 'dotenv/config';
 
 async function getAccessToken(authCode: string): Promise<string> {
     try {
-        const response = await axios.post('https://api.mercadopago.com/oauth/token', {
-            client_id: process.env.MERCADO_PAGO_CLIENT_ID,
-            client_secret: process.env.MERCADO_PAGO_CLIENT_SECRET,
-            grant_type: 'authorization_code',
-            code: authCode,
-            redirect_uri: `${process.env.BASE_URL}/sallercallback` 
+        const response = await axios.post('https://api.mercadopago.com/oauth/token', null, {
+            params: {
+                client_id: process.env.MERCADO_PAGO_CLIENT_ID,
+                client_secret: process.env.MERCADO_PAGO_CLIENT_SECRET,
+                grant_type: 'authorization_code',
+                code: authCode,
+                redirect_uri: `${process.env.BASE_URL}/sallercallback`
+            }
         });
 
         return response.data.access_token;
     } catch (error) {
-        console.error('Erro ao obter o access_token:', error);
+        console.error('Erro ao obter o access_token:', error.response?.data || error.message);
         throw error;
     }
 }
 
-export default getAccessToken
+export default getAccessToken;
