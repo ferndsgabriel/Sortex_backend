a3bb40d1bbd22a53b3e21e1321a33aadc0a48549
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-08-17 15:52:29 -0300

    trocando o axios pelo node fetch

--- src/mercadopago/axiosToken.ts
+++ src/mercadopago/axiosToken.ts
@@ -1,26 +1,42 @@
-import axios from 'axios';
-
-const clientId = '3875468438633898';
-const clientSecret = 'eq3X60nI9yfXl2lixEIa6ITMfM7HbmYS';
+import fetch from 'node-fetch';
 
 
-const getAccessToken = async (authCode: string) => {
+interface authCodeProps {
+    access_token: string,
+    token_type: string,
+    expires_in: number,
+    scope: string,
+    user_id: number,
+    refresh_token: string,
+    public_key: string,
+    live_mode: boolean
+}
+
+const getAccessToken = async (authCode:string) => {
     try {
-        const response = await axios.post('https://api.mercadopago.com/oauth/token', {
-            client_id: clientId,
-            client_secret: clientSecret,
-            grant_type:'authorization_code',
-            redirect_uri: 'https://sortexbackend.vercel.app/sallercallback'
-        }, {
+        const response = await fetch('https://api.mercadopago.com/oauth/token', {
+            method: 'POST',
             headers: {
-                'Content-Type': 'Content-Type: application/json'
-            }
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                client_id: '3875468438633898',
+                client_secret: 'eq3X60nI9yfXl2lixEIa6ITMfM7HbmYS',
+                code: authCode,
+                grant_type: 'authorization_code',
+                redirect_uri: 'https://sortexbackend.vercel.app/sallercallback'
+            })
         });
 
-        return response.data.access_token;
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data : any  = await response.json();
+        return data.access_token ;
     } catch (error) {
-        console.error('Error getting access token:', error.response ? error.response.data : error.message);
-        throw error; // Re-throw the error to be caught in the caller
+        console.error('Error getting access token:', error.message);
+        throw error;
     }
 };
 
