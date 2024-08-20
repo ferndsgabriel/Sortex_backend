a02cfec4d52eff39676fe0bd448dde2bdae3f8d9
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-08-20 11:16:51 -0300

    fix: remove secret do codigo

--- src/mercadopago/axiosPayment.ts
+++ src/mercadopago/axiosPayment.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import "dotenv/config";
 
-const accessToken = 'APP_USR-3875468438633898-081711-c309e7f4bcf3481cac8562b69a9869b4-247395576'
+const accessToken = process.env.MERCADO_PAG0_ACCESS_TOKEN;
 
 const axiosPayment = async (paymentId: string) => {
     const url = `https://api.mercadopago.com/v1/payments/${paymentId}`;
